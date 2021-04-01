94cdb35130c60a0929e20d8ebdb95bd6a0c35cbe
loyivir <loyivir@gmail.com>
2021-04-01 19:17:00 +0300

    Add portfolio section

--- js/main.js
+++ js/main.js
@@ -5,4 +5,47 @@ let menuButton = document.querySelector(".header__menu");
 menuButton.addEventListener("click", function(){  
   document.querySelector(".header__nav").classList.toggle("header__nav_visible");
 });
+
+  const portfolioSwiper = new Swiper('.portfolio-swiper', {
+  // Optional parameters
+  loop: false,
+  slidesPerView: 4,   
+  slidesPerGroup: 1,  
+      centeredSlides: false,
+      direction: 'vertical',
+      slidesPerGroupSkip: 0,
+  spaceBetween: 0,
+  keyboard: {        
+        enabled: true,
+        onlyInViewport: true,
+      },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.portfolio__button_next',
+    prevEl: '.portfolio__button_prev',
+  },
+  breakpoints:     
+       {
+        992: {
+          slidesPerView: 'auto',
+          spaceBetween: 30,
+          direction: 'horizontal',
+           centeredSlides: false,
+        },
+         870: {
+          slidesPerView: 'auto',
+          spaceBetween: 30,
+          direction: 'horizontal',
+           centeredSlides: false,
+        },
+        768: {
+          slidesPerView: 'auto',
+          spaceBetween: 30,
+          direction: 'horizontal',
+             centeredSlides: false,
+             slidesPerGroupSkip: 1,
+        },
+      }
+});
+// Initialize portfolio slider
 });
