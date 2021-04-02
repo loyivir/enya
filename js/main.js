683ece51caa35e8b8381c0515b194657bc281996
loyivir <loyivir@gmail.com>
2021-04-02 19:03:42 +0300

    Add feedbacks slider

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ menuButton.addEventListener("click", function(){
   // Optional parameters
   loop: false,
   slidesPerView: 4,   
-  slidesPerGroup: 1,  
+  slidesPerGroup: 4,  
       centeredSlides: false,
       direction: 'vertical',
       slidesPerGroupSkip: 0,
@@ -30,22 +30,67 @@ menuButton.addEventListener("click", function(){
           slidesPerView: 'auto',
           spaceBetween: 30,
           direction: 'horizontal',
-           centeredSlides: false,
+           centeredSlides: true,
+           slidesPerGroupSkip: 0,
+             slidesPerGroup: 1, 
         },
          870: {
           slidesPerView: 'auto',
           spaceBetween: 30,
           direction: 'horizontal',
-           centeredSlides: false,
+           centeredSlides: true,
+           slidesPerGroupSkip: 0,
+             slidesPerGroup: 1, 
         },
         768: {
           slidesPerView: 'auto',
           spaceBetween: 30,
-          direction: 'horizontal',
-             centeredSlides: false,
-             slidesPerGroupSkip: 1,
+          direction: 'horizontal',            
+             slidesPerGroupSkip: 0,
+             slidesPerGroup: 1,  
+             centeredSlides: true,
         },
       }
 });
+portfolioSwiper.slideNext(0, false);
 // Initialize portfolio slider
+  const feedbacksSwiper = new Swiper('.feedbacks-swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,   
+  slidesPerGroup: 1,  
+    
+  keyboard: {        
+        enabled: true,
+        onlyInViewport: true,
+      },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.feedbacks__button_next',
+    prevEl: '.feedbacks__button_prev',
+  },
+  breakpoints:     
+       {
+          1200: {
+          slidesPerView: 2,     
+          slidesPerGroup: 2, 
+          spaceBetween: 30,
+        },
+
+        992: {
+          slidesPerView: 1,        
+          slidesPerGroup: 1, 
+        },
+         870: {
+          slidesPerView: 1,         
+           slidesPerGroup: 1, 
+        },
+        768: {
+          slidesPerView: 1,        
+          slidesPerGroup: 1,  
+            
+        },
+      }
+});
+// Initialize feedbacks slider
 });
